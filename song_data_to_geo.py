a8d1100bed7bbb232f8fb8e44f7767931fff4e8d
drunken-boat <rainylty@gmail.com>
2022-09-18 14:49:48 +0800

    add coords

--- song_data_to_geo.py
+++ song_data_to_geo.py
@@ -1,2 +1,9 @@
 import pandas as pd
+import geopandas as gpd
+import seaborn
 
+db = gpd.read_file("/Users/rainylty/STUDY/IVth/datascience/summer_school/summoning_and_lotr/Viz_of_Summoning_and_Middle_Earth/final_geo_data/with_coords.shp")
+
+db.info
+
+seaborn.jointplot(x='xcoord',y='ycoord', data=db, s=0.5)
